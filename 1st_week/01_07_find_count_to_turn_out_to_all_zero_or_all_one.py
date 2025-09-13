66242bb4eeb51e924ddf7bf5d9df0fcf2da8620a
Mindun <pmk10077@gmail.com>
2025-09-13 20:23:44 +0900

    string의 가장 앞 글자의 경우의 수

--- 1st_week/01_07_find_count_to_turn_out_to_all_zero_or_all_one.py
+++ 1st_week/01_07_find_count_to_turn_out_to_all_zero_or_all_one.py
@@ -6,12 +6,19 @@ def find_count_to_turn_out_to_all_zero_or_all_one(string):
     count_to_all_zero = 0
     count_to_all_one = 0
 
+    # string의 가장 앞 글자의 경우의 수
+    if string[0] == '0':
+        count_to_all_one += 1
+    elif string[0] == '0':
+        count_to_all_zero += 1
+
+
     for n in range(len(string)-1):
-        print("string[n-1] ",string[n-1] ,"string[n]",string[n])
-        if string[n] != string[n+1] and string[n+1] == "1":
+        if string[n] != string[n + 1] and string[n + 1] == "1":
             count_to_all_zero += 1
-        elif string[n] != string[n+1] and string[n+1] == "0":
+        elif string[n] != string[n + 1] and string[n + 1] == "0":
             count_to_all_one += 1
+
     return min(count_to_all_zero, count_to_all_one)
 
 
